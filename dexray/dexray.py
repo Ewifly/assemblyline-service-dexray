ec1a0bbb31ce6d5603931b9e555dba2d6fb39f78
Ewi <ewifly@gmail.com>
2021-05-07 10:49:21 +0200

    test with cwd

--- dexray/dexray.py
+++ dexray/dexray.py
@@ -29,17 +29,26 @@ class Dexray(ServiceBase):
 
             self.log.info(f"workingdir = {self.working_directory}")
             self.log.info(f"filename = {request.file_name}")
-            
-            path = request.file_path
-            self.log.info(f"path = {path}")
-            lnewfile_path = glob.glob(path + ".*")
+            cwd = self.working_directory
+
+            lnewfile_path = glob.glob(cwd + request.file_name + ".*")
             self.log.info(f"newfile path = {lnewfile_path}")
             if lnewfile_path:
                 newfile_path = lnewfile_path[0]
                 newfile_name = newfile_path.split("/")[-1]
                 self.log.info(f"newfile_name = {newfile_name}")
-                request.add_extracted(path, newfile_name, "resubmit")
+                request.add_extracted(cwd, newfile_name, "resubmit")
                 self.log.info(f"file resubmitted")
+            # path = request.file_path
+            # self.log.info(f"path = {path}")
+            # lnewfile_path = glob.glob(path + ".*")
+            # self.log.info(f"newfile path = {lnewfile_path}")
+            # if lnewfile_path:
+            #     newfile_path = lnewfile_path[0]
+            #     newfile_name = newfile_path.split("/")[-1]
+            #     self.log.info(f"newfile_name = {newfile_name}")
+            #     request.add_extracted(path, newfile_name, "resubmit")
+            #     self.log.info(f"file resubmitted")
 
         request.result = result
         
