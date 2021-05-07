fd3852ad0eabe6a0a9e45818e3f43bfd6fe6669b
Ewi <ewifly@gmail.com>
2021-05-07 15:50:32 +0200

    added toolpath in manifest + log fixes

--- Dockerfile
+++ Dockerfile
@@ -5,7 +5,10 @@ ENV SERVICE_PATH dexray.dexray.Dexray
 USER root
 
 # Install any service dependencies here
-RUN apt-get update && apt-get install -y perl libcrypt-rc4-perl libdigest-crc-perl libcrypt-blowfish-perl libole-storage-lite-perl 
+RUN apt-get update && apt-get install -y perl libcrypt-rc4-perl libdigest-crc-perl libcrypt-blowfish-perl libole-storage-lite-perl
+RUN mkdir -p /opt/al_support
+RUN mv dexray/dexray.pl /opt/al_support
+RUN chmod +x /opt/al_support/dexray.pl
 # Switch to assemblyline user
 USER assemblyline
 
--- dexray/dexray.py
+++ dexray/dexray.py
@@ -2,6 +2,7 @@ import os, string, tempfile, glob
 from pathlib import Path
 from os import listdir
 from os.path import isfile, join
+from subprocess import Popen, PIPE, call
 
 from assemblyline.common.str_utils import safe_str
 from assemblyline_v4_service.common.base import ServiceBase
@@ -11,51 +12,56 @@ from yaml import add_constructor
 class Dexray(ServiceBase):
     def __init__(self, config=None):
         super(Dexray, self).__init__(config)
-
+        self.dexraytool = self.config.get("dexray_tool_path", None)
     def start(self):
         self.log.info(f"start() from {self.service_attributes.name} service called")
 
     def execute(self, request):
-
+        self.log.info(f"tool path = {self.dexraytool}")
+        file = request.file_path
+        assert(file)
+        cwd = self.working_directory
+        assert(cwd)
+        filename = request.file_name
+        assert(filename)
+        self.log.info(f"file = {file}")
         result = Result()
         text_section = ResultSection('DexRay logs :')
-
-        my_cmd = "perl dexray/dexray.pl " + request.file_path
-        my_cmd_output = os.popen(my_cmd)
+        # my_cmd = "perl" + os.getcwd() + "dexray/dexray.pl " + request.file_path
+        unquar = Popen(["perl", self.dexraytool, file])
+        _ = unquar.communicate()
         text_section.add_line("aftr cmd")
-        if my_cmd_output:
-            text_section.add_line("command success")
-            self.log.info(f"command success")
-            cwd = self.working_directory
-            filename = request.file_name
-            self.log.info(f"workingdir = {cwd}")
-            text_section.add_line("workingdir = ", cwd)
-            self.log.info(f"filename = {filename}")
-            text_section.add_line("file name = ", filename)
+        text_section.add_line("command success")
+        self.log.info(f"command success")
 
-            lnewfile_path = glob.glob(cwd + filename + ".*")
-            self.log.info(f"newfile path = {lnewfile_path}")
-            if lnewfile_path:
-                text_section.add_line("in cwd")
-                text_section.add_line()
-                newfile_path = lnewfile_path[0]
-                newfile_name = os.path.basename(newfile_path)
-                self.log.info(f"newfile_name = {newfile_name}")
-                request.add_extracted(cwd, newfile_name, "resubmit")
-                text_section.add_line(f"file resubmitted as: {newfile_name}")
-                self.log.info(f"file resubmitted")
-            path = request.file_path
-            self.log.info(f"path = {path}")
-            lnewfile_path = glob.glob(path + ".*")
-            self.log.info(f"newfile path = {lnewfile_path}")
-            if lnewfile_path:
-                text_section.add_line("in filepath")
-                newfile_path = lnewfile_path[0]
-                newfile_name = os.path.basename(newfile_path)
-                self.log.info(f"newfile_name = {newfile_name}")
-                request.add_extracted(path, newfile_name, "resubmit")
-                text_section.add_line(f"file resubmitted as: {newfile_name}")
-                self.log.info(f"file resubmitted")
+        self.log.info(f"workingdir = {cwd}")
+        text_section.add_line(f"workingdir = {cwd}")
+        self.log.info(f"filename = {filename}")
+        text_section.add_line(f"file name = {filename}")
+
+        lnewfile_path = glob.glob(cwd + filename + ".*")
+        self.log.info(f"newfile path = {lnewfile_path}")
+        if lnewfile_path:
+            text_section.add_line("in cwd")
+            text_section.add_line()
+            newfile_path = lnewfile_path[0]
+            newfile_name = os.path.basename(newfile_path)
+            self.log.info(f"newfile_name = {newfile_name}")
+            request.add_extracted(cwd, newfile_name, "resubmit")
+            text_section.add_line(f"file resubmitted as: {newfile_name}")
+            self.log.info(f"file resubmitted")
+        path = request.file_path
+        self.log.info(f"path = {path}")
+        lnewfile_path = glob.glob(path + ".*")
+        self.log.info(f"newfile path = {lnewfile_path}")
+        if lnewfile_path:
+            text_section.add_line("in filepath")
+            newfile_path = lnewfile_path[0]
+            newfile_name = os.path.basename(newfile_path)
+            self.log.info(f"newfile_name = {newfile_name}")
+            request.add_extracted(path, newfile_name, "resubmit")
+            text_section.add_line(f"file resubmitted as: {newfile_name}")
+            self.log.info(f"file resubmitted")
 
         request.result = result
         
