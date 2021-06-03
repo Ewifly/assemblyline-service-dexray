d04a7a23ba38b8acdc0cf89e09f27109c8db87e4
Ewifly <ewifly@gmail.com>
2021-06-03 16:04:16 +0200

    IO changes from sgron cccs

--- dexray/dexray.py
+++ dexray/dexray.py
@@ -1,47 +1,34 @@
-import os, string, tempfile, glob, sys
-from pathlib import Path
-from os import listdir
-from os.path import isfile, join
-from subprocess import Popen, PIPE, call
+import os
+import glob
+from subprocess import Popen
 
-from assemblyline.common.str_utils import safe_str
 from assemblyline_v4_service.common.base import ServiceBase
-from assemblyline_v4_service.common.result import Result, ResultSection, BODY_FORMAT
-from yaml import add_constructor
+from assemblyline_v4_service.common.result import Result, ResultSection
+
 
 class Dexray(ServiceBase):
     def __init__(self, config=None):
         super(Dexray, self).__init__(config)
         self.dexraytool = self.config.get("dexray_tool_path", None)
+
     def start(self):
-        self.log.info(f"start() from {self.service_attributes.name} service called")
+        if not os.path.exists(self.dexraytool):
+            self.log.error(f"Could not find dexray perl script at: {self.dexraytool}")
 
     def execute(self, request):
-        self.log.info(f"tool path = {self.dexraytool}")
         file = request.file_path
-        cwd = self.working_directory
-        filename = request.file_name
-        self.log.info(f"file = {file}")
         result = Result()
-        text_section = ResultSection('DexRay logs :')
         unquar = Popen(["perl", self.dexraytool, file])
         unquar.wait()
-        self.log.info(f"command success")
 
-        self.log.info(f"workingdir = {cwd}")
-        self.log.info(f"filename = {filename}")
-
-        path = request.file_path
-        lnewfile_path = glob.glob(path + ".*") #should find the newly created file (filename.<offset>_<AV Name>.out)
-        self.log.info(f"newfile path = {lnewfile_path}")
+        lnewfile_path = glob.glob(file + ".*")  # should find the newly created file (filename.<offset>_<AV Name>.out)
         if lnewfile_path:
-            newfile_path = lnewfile_path[0]
-            newfile_name = os.path.basename(newfile_path)
-            self.log.info(f"newfile_name = {newfile_name}")
-            request.add_extracted(path, newfile_name, "resubmit")
-            request.add_supplementary(path, newfile_name, "extracted file")
-            text_section.add_line(f"file resubmitted as: {newfile_name}")
-            self.log.info(f"file resubmitted")
+            text_section = ResultSection('DexRay found files:')
+            for newfile_path in lnewfile_path:
+                vendor = newfile_path.rsplit("_", 1)[1].split(".out")[0]
+                offset = newfile_path.rsplit("_", 1)[0].rsplit(".", 1)[1]
+                newfile_name = f"{request.file_name}.{offset}_{vendor}"
+                request.add_extracted(newfile_path, newfile_name, f"{vendor} un-quarantined file")
+                text_section.add_line(f"Resubmitted {vendor} un-quarantined file as : {newfile_name}")
             result.add_section(text_section)
         request.result = result
-        
